0a3860fb36c695e62d3b47368b48a4e6401f6e64
Mariana Mendes da Silva <mariana.mendes98@hotmail.com>
2016-10-12 20:14:58 -0300

    Questão 1

--- exercicio01.c
+++ exercicio01.c
@@ -1,13 +1,15 @@
+
 /* Questão 1 - a) Algoritmo que recebe uma matriz de >=2 e verifica se ela é uma matriz de Vandermonde
 b)Calcula norma de Frobenius, norma linha, norma coluna de A e produto interno entre a primeira linha e a segunda coluna, bem como o ângulo entre esses dois vetores.
-c)
+c)Calcula o determinante da matriz inserida usando a fórmula específica para matrizes de Vandermonde
 */
 
 #include <stdio.h>
 #include <math.h>
 
-void verificaVandermonde (int ordem, int matriz[ordem][ordem]){
-	int i, j, aux, verifica = 1;
+int verificaVandermonde (int ordem, float matriz[ordem][ordem]){
+	int i, j, verifica = 1;
+	float aux;
 	
 	for(i = 0; i < ordem; i++){
 		if(matriz[i][0] != 1){
@@ -27,14 +29,11 @@ void verificaVandermonde (int ordem, int matriz[ordem][ordem]){
 	  	}	 
 	}
 		
-	if(verifica)	
-		printf("A matriz inserida eh matriz de Vandermonde.\n");
-	else
-		printf("A matriz inserida nao eh matriz de Vandermonde.\n");
+	return verifica;
 }
 
-void normaFrobenius(int ordem, int matriz[ordem][ordem]){
-	int aux = 0;
+void normaFrobenius(int ordem, float matriz[ordem][ordem]){
+	float aux = 0;
 	int i, j;
 	float resultado;
 	
@@ -52,16 +51,17 @@ void normaFrobenius(int ordem, int matriz[ordem][ordem]){
 	printf("A norma de Frobenius da matriz inserida eh igual a: %f\n", resultado);
 }
 
-int modulo(int numero){
+float modulo(float numero){
 	if(numero > 0)
 		return numero;
 	else
 		return -(numero);
 }
 
-void normaLinha(int ordem, int matriz[ordem][ordem]){
+void normaLinha(int ordem, float matriz[ordem][ordem]){
 	
-	int i, j, aux = 0, maiorsoma = 0;
+	int i, j;
+	float aux = 0, maiorsoma = 0;
 	
 	for(i = 0; i < ordem; i++)
  	{
@@ -73,16 +73,18 @@ void normaLinha(int ordem, int matriz[ordem][ordem]){
 		if(aux > maiorsoma)
 		{
 			maiorsoma = aux;
-			aux = 0;
 		}
+		
+		aux = 0;
   	}
   	
-  	printf("A norma linha da matriz inserida eh igual a: %d\n", maiorsoma);
+  	printf("A norma linha da matriz inserida eh igual a: %f\n", maiorsoma);
 }
 
-void normaColuna(int ordem, int matriz[ordem][ordem]){
+void normaColuna(int ordem, float matriz[ordem][ordem]){
 		
-	int i, j, aux = 0, maiorsoma = 0;
+	int i, j; 
+	float aux = 0, maiorsoma = 0;
 	
 	for(i = 0; i < ordem; i++)
  	{
@@ -94,15 +96,17 @@ void normaColuna(int ordem, int matriz[ordem][ordem]){
 		if(aux > maiorsoma)
 		{
 			maiorsoma = aux;
-			aux = 0;
 		}
+		
+		aux = 0;
   	}
   	
-  	printf("A norma coluna da matriz inserida eh igual a: %d\n", maiorsoma);
+  	printf("A norma coluna da matriz inserida eh igual a: %f\n", maiorsoma);
 }
 
-int produtoInterno(int ordem, int matriz[ordem][ordem]){
-	int i, produtoria = 0;
+float produtoInterno(int ordem, float matriz[ordem][ordem]){
+	int i;
+	float produtoria = 0;
 	
 	for(i = 0; i < ordem; i++)
 	{
@@ -112,8 +116,9 @@ int produtoInterno(int ordem, int matriz[ordem][ordem]){
 	return produtoria;	
 }
 
-float moduloVetorLinha(int ordem, int matriz[ordem][ordem]){
-	int i, modulo = 0;
+float moduloVetorLinha(int ordem, float matriz[ordem][ordem]){
+	int i; 
+	float modulo = 0;
 	
 	for(i = 0; i < ordem; i++)
 	{
@@ -123,8 +128,9 @@ float moduloVetorLinha(int ordem, int matriz[ordem][ordem]){
 	return sqrt(modulo);	
 }
 
-float moduloVetorColuna(int ordem, int matriz[ordem][ordem]){
-	int i, modulo = 0;
+float moduloVetorColuna(int ordem, float matriz[ordem][ordem]){
+	int i; 
+	float modulo = 0;
 	
 	for(i = 0; i < ordem; i++)
 	{
@@ -134,7 +140,7 @@ float moduloVetorColuna(int ordem, int matriz[ordem][ordem]){
 	return sqrt(modulo);	
 }
 
-void anguloDe2Vetores(int ordem, int matriz[ordem][ordem]){
+void anguloDe2Vetores(int ordem, float matriz[ordem][ordem]){
 	float cossenoTeta = 0;
 	
 	cossenoTeta = (float)produtoInterno(ordem, matriz) / (moduloVetorLinha(ordem, matriz) * moduloVetorColuna(ordem, matriz));
@@ -153,6 +159,23 @@ void anguloDe2Vetores(int ordem, int matriz[ordem][ordem]){
 		printf("Angulo entre os dois vetores eh igual ao arc cos(%f)\n", cossenoTeta);	
 }
 
+void determinanteVandermonde(int ordem, float matriz[ordem][ordem]){
+	
+	if(verificaVandermonde(ordem, matriz)){
+		
+		int i, j = 1, k;
+		float produto = 1;
+			
+		for (i=1; i<ordem; i++){
+			for(k=0; k<i; k++){
+				produto *= (matriz[i][j] - matriz[k][j]);
+			}		
+		}
+		
+		printf("O determinante da matriz inserida eh: %f \n", produto);
+	}else printf("Nao eh possivel calcular o determinante pela regra das matrizes de Vandermonde, pois a matriz nao eh de Vandermonde.");
+}
+
 int main(void)
 {
 	int ordem, i, j;
@@ -164,7 +187,7 @@ int main(void)
 		printf("A matriz a ser inserida nao pode ter ordem inferior a 2.\n");
 	else
 	{
-		int matriz[ordem][ordem];
+		float matriz[ordem][ordem];
 		
 		printf("Insira a matriz: \n");
 		
@@ -172,20 +195,24 @@ int main(void)
 		{ 
 			for(j = 0; j < ordem; j++)
 		  	{
-		   		scanf("%d", &matriz[i][j]);
+		   		scanf("%f", &matriz[i][j]);
 			}	 
 		}
 		
 		printf("----------- Resultado -----------\n");
 			
-		verificaVandermonde(ordem, matriz);
+		if(verificaVandermonde(ordem, matriz))	
+			printf("A matriz inserida eh matriz de Vandermonde.\n");
+		else
+			printf("A matriz inserida nao eh matriz de Vandermonde.\n");	
+		
 		normaFrobenius(ordem, matriz);
 		normaLinha(ordem, matriz);
 		normaColuna(ordem, matriz);
-		printf("O produto interno da 1a linha com a 2a coluna da matriz inserida eh igual a: %d \n", produtoInterno(ordem, matriz));
+		printf("O produto interno da 1a linha com a 2a coluna da matriz inserida eh igual a: %f \n", produtoInterno(ordem, matriz));
 		anguloDe2Vetores(ordem, matriz);
+		determinanteVandermonde(ordem, matriz);
 		
 	}	
 	return 0;
 }
-
